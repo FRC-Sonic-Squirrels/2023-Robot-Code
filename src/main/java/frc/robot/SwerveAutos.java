07422c2a273ba9372c611d8beff0493f7e09c1ed
Tim <92405837+CodeCreator3@users.noreply.github.com>
2023-01-14 10:30:54 -0800

    working on eventMarkers

--- src/main/java/frc/robot/SwerveAutos.java
+++ src/main/java/frc/robot/SwerveAutos.java
@@ -2,11 +2,16 @@ package frc.robot;
 
 import static frc.robot.subsystems.drivetrain.DrivetrainConstants.*;
 
+import java.util.HashMap;
+
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.FollowPath;
+import frc.robot.commands.FollowPathWithEvents;
+import frc.robot.commands.TeleopSwerve;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.intake.Intake;
 
@@ -66,5 +71,21 @@ public class SwerveAutos {
         AUTO_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED);
 
         return new SequentialCommandGroup(new FollowPath(path, drivetrain, true));
+  }
+
+
+  public Command 2mForwardWithIntake() {
+    HashMap<String, Command> eventMap = new HashMap<>();
+
+    eventMap.put("extendIntake", this.intake.extendCommand());
+    eventMap.put("retractIntake", this.intake.retractCommand());
+
+    PathPlannerTrajectory path = 
+      PathPlanner.loadPath(
+        "2mForwardWithIntake", 
+        AUTO_MAX_SPEED_METERS_PER_SECOND, 
+        AUTO_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED);
+
+        return new SequentialCommandGroup(new FollowPathWithEvents(new FollowPath(path, drivetrain, true), path.getMarkers(), eventMap));
   }
 }
