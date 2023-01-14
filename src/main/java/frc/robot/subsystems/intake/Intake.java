e05aef5a08326e5ca2d9e7ae1b1d11d83f8a4ad8
Tim <92405837+CodeCreator3@users.noreply.github.com>
2023-01-14 10:31:17 -0800

    working on eventMarkers

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -4,6 +4,9 @@
 
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.intake.IntakeIO.IntakeIOInputs;
 import org.littletonrobotics.junction.Logger;
@@ -39,5 +42,17 @@ public class Intake extends SubsystemBase {
 
   public void retract() {
     io.setExtended(false);
+  }
+
+  public Command extendCommand(){
+    return new InstantCommand(() -> {
+      extend();
+  });
+  }
+
+  public Command retractCommand(){
+    return new InstantCommand(() -> {
+      retract();
+  });
   }
 }
