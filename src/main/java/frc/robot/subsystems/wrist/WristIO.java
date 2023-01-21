5b98bc6c8051dfad3998d80ac16746b3d7fc061a
Soliana Aderaw <solianaby@gmail.com>
2023-01-21 13:37:39 -0800

    wrist update

--- src/main/java/frc/robot/subsystems/wrist/WristIO.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIO.java
@@ -1,25 +1,25 @@
 package frc.robot.subsystems.wrist;
+
 import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 
-
-public interface WristIO extends LoggableInputs {
+public interface WristIO {
+  public static class WristIOInputs implements LoggableInputs {
 
     public boolean extended = false;
 
     public void toLog(LogTable table) {
-        table.put("Extended", extended);
+      table.put("Extended", extended);
     }
 
-        public void fromLog(LogTable table) {
-            extended = table.getBoolean("Extended", extended);
-      
+    public void fromLog(LogTable table) {
+      extended = table.getBoolean("Extended", extended);
+    }
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(WristIOInputs inputs) {}
+
+  /** Set solenoid state. */
+  public default void setExtended(boolean extended) {}
 }
-}
-
- /** Updates the set of loggable inputs. */
- public default void updateInputs(WristIOInputs inputs) {}
-
- /** Set solenoid state. */
- public default void setExtended(boolean extended) {}
-
--- src/main/java/frc/robot/subsystems/wrist/WristIOSolenoid.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIOSolenoid.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 
 public class WristIOSolenoid implements WristIO {
-  Solenoid solenoid = null;
+  private Solenoid solenoid;
 
   public void Wrist() {
 
@@ -12,12 +12,11 @@ public class WristIOSolenoid implements WristIO {
   }
 
   // TODO: figure out a way to use the constants WRIST_SOLENOID_DEPLOY and WRIST_SOLENOID_RETRACT
-  @Override
+
   public void solenoidUp() {
     solenoid.set(true);
   }
 
-  @Override
   public void solenoidDown() {
     solenoid.set(true);
   }
