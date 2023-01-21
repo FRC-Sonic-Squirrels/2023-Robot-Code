15c1696f1edd4b3a7759e8d08b93695fb4390a85
Soliana Aderaw <solianaby@gmail.com>
2023-01-21 13:14:29 -0800

    wrist update 3

--- src/main/java/frc/robot/subsystems/wrist/WristIOSolenoid.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIOSolenoid.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.wrist;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 
-public class WristIOFalcon implements WristIO {
+public class WristIOSolenoid implements WristIO {
   Solenoid solenoid = null;
 
   public void Wrist() {
@@ -12,10 +12,12 @@ public class WristIOFalcon implements WristIO {
   }
 
   // TODO: figure out a way to use the constants WRIST_SOLENOID_DEPLOY and WRIST_SOLENOID_RETRACT
+  @Override
   public void solenoidUp() {
     solenoid.set(true);
   }
 
+  @Override
   public void solenoidDown() {
     solenoid.set(true);
   }
