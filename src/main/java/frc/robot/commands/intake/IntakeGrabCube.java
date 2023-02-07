dc4bc0b03e3d2a884c8da33b51596c2369bf63df
Soliana Aderaw <solianaby@gmail.com>
2023-02-06 20:16:00 -0800

    continue Intake commands

---
+++ src/main/java/frc/robot/commands/intake/IntakeGrabCube.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.Intake;
+
+public class IntakeGrabCube extends CommandBase {
+  /** Creates a new IntakeGrabCube. */
+
+
+  Intake intake;
+
+  double speed;
+
+
+  public IntakeGrabCube(Intake intake) {
+    
+    this(intake, 0.8);
+
+  }
+
+  public IntakeGrabCube(Intake intake, double speed) {
+
+    this.intake = intake;
+    this.speed = speed;
+
+    addRequirements(intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    intake.intakeCube(speed);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
+
+
