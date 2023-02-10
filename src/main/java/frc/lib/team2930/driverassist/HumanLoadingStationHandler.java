23b904edd4c224eadd01a84fc101e82e514efa53
LuminousLlama <keyushattarde@gmail.com>
2023-02-09 22:07:40 -0800

    tuning poses

--- src/main/java/frc/lib/team2930/driverassist/HumanLoadingStationHandler.java
+++ src/main/java/frc/lib/team2930/driverassist/HumanLoadingStationHandler.java
@@ -9,15 +9,27 @@ import java.util.ArrayList;
 
 public class HumanLoadingStationHandler {
 
+  // this checkpoint is where the elevator will extend up
+  private static final double LAST_CHECKPOINT_X = 15.15;
+  private static final double LEFT_SIDE_Y = 7.5;
+  private static final double RIGHT_SIDE_Y = 6.0;
+
   public final PoseAndHeading[] checkpointsOutsideIn;
   public final PoseAndHeading finalPickupPose;
 
   private static HumanLoadingStationHandler BLUE_ALLIANCE_LEFT =
       new HumanLoadingStationHandler(
           new PoseAndHeading(
-              new Pose2d(15.7, 7.5, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0)),
+              new Pose2d(15.7, LEFT_SIDE_Y, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0)),
+
+          // checkpoints
           new PoseAndHeading(
-              new Pose2d(12.75, 7.4, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0))
+              new Pose2d(10.35, LEFT_SIDE_Y, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0)),
+          new PoseAndHeading(
+              new Pose2d(12.75, LEFT_SIDE_Y, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0)),
+          new PoseAndHeading(
+              new Pose2d(LAST_CHECKPOINT_X, LEFT_SIDE_Y, Rotation2d.fromDegrees(0)),
+              Rotation2d.fromDegrees(0))
           // more points later
 
           );
@@ -25,18 +37,27 @@ public class HumanLoadingStationHandler {
   private static HumanLoadingStationHandler BLUE_ALLIANCE_RIGHT =
       new HumanLoadingStationHandler(
           new PoseAndHeading(
-              new Pose2d(15.7, 6.25, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0)),
+              new Pose2d(15.7, RIGHT_SIDE_Y, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0)),
+
+          // checkpoints
           new PoseAndHeading(
-              new Pose2d(12.75, 6.25, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0))
+              new Pose2d(10.35, RIGHT_SIDE_Y, Rotation2d.fromDegrees(0)),
+              Rotation2d.fromDegrees(0)),
+          new PoseAndHeading(
+              new Pose2d(12.75, RIGHT_SIDE_Y, Rotation2d.fromDegrees(0)),
+              Rotation2d.fromDegrees(0)),
+          new PoseAndHeading(
+              new Pose2d(LAST_CHECKPOINT_X, RIGHT_SIDE_Y, Rotation2d.fromDegrees(0)),
+              Rotation2d.fromDegrees(0))
           // more points later
 
           );
   ;
 
   private static HumanLoadingStationHandler RED_ALLIANCE_RIGHT =
-      new HumanLoadingStationHandler(BLUE_ALLIANCE_RIGHT);
-  private static HumanLoadingStationHandler RED_ALLIANCE_LEFT =
       new HumanLoadingStationHandler(BLUE_ALLIANCE_LEFT);
+  private static HumanLoadingStationHandler RED_ALLIANCE_LEFT =
+      new HumanLoadingStationHandler(BLUE_ALLIANCE_RIGHT);
 
   private HumanLoadingStationHandler(
       PoseAndHeading finalPickupPose, PoseAndHeading... checkpointsOutsideIn) {
@@ -51,7 +72,8 @@ public class HumanLoadingStationHandler {
     ArrayList<PoseAndHeading> checkpointsList = new ArrayList<PoseAndHeading>();
 
     for (PoseAndHeading poseAndHeading : blueToFlip.checkpointsOutsideIn) {
-      checkpointsList.add(poseAndHeading);
+      var flipped = flipForRed(poseAndHeading);
+      checkpointsList.add(flipped);
     }
 
     this.checkpointsOutsideIn = checkpointsList.toArray(new PoseAndHeading[0]);
@@ -132,7 +154,8 @@ public class HumanLoadingStationHandler {
     }
   }
 
-  public static void log(String path) {
-    // Logger.getInstance().recordOutput(path + "/BLUE_LEFT"/, value);
+  public static void log(String path, HumanLoadingStationHandler station) {
+    // Logger.getInstance()
+    //     .recordOutput(path + "/BLUE_LEFT/checkpoints", station.checkpointsOutsideIn);
   }
 }
