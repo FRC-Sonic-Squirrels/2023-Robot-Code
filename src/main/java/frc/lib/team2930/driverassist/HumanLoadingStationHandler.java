77f60e9752d77a8f945a2faf75f61e975e43f9d2
LuminousLlama <keyushattarde@gmail.com>
2023-02-09 20:43:05 -0800

    human player station checkpoints

--- src/main/java/frc/lib/team2930/driverassist/HumanLoadingStationHandler.java
+++ src/main/java/frc/lib/team2930/driverassist/HumanLoadingStationHandler.java
@@ -1,5 +1,7 @@
 package frc.lib.team2930.driverassist;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.lib.team2930.driverassist.GridPositionHandler.PoseAndHeading;
@@ -10,15 +12,61 @@ public class HumanLoadingStationHandler {
   public final PoseAndHeading[] checkpointsOutsideIn;
   public final PoseAndHeading finalPickupPose;
 
-  private static HumanLoadingStationHandler BLUE_ALLIANCE_RIGHT;
-  private static HumanLoadingStationHandler BLUE_ALLIANCE_LEFT;
+  private static HumanLoadingStationHandler BLUE_ALLIANCE_LEFT =
+      new HumanLoadingStationHandler(
+          new PoseAndHeading(
+              new Pose2d(15.7, 7.5, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0)),
+          new PoseAndHeading(
+              new Pose2d(12.75, 7.4, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0))
+          // more points later
 
-  private static HumanLoadingStationHandler RED_ALLIANCE_RIGHT;
-  private static HumanLoadingStationHandler RED_ALLIANCE_LEFT;
+          );
 
-  private HumanLoadingStationHandler(PoseAndHeading[] checkpoints, PoseAndHeading pickupPose) {
-    checkpointsOutsideIn = null;
-    finalPickupPose = null;
+  private static HumanLoadingStationHandler BLUE_ALLIANCE_RIGHT =
+      new HumanLoadingStationHandler(
+          new PoseAndHeading(
+              new Pose2d(15.7, 6.25, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0)),
+          new PoseAndHeading(
+              new Pose2d(12.75, 6.25, Rotation2d.fromDegrees(0)), Rotation2d.fromDegrees(0))
+          // more points later
+
+          );
+  ;
+
+  private static HumanLoadingStationHandler RED_ALLIANCE_RIGHT =
+      new HumanLoadingStationHandler(BLUE_ALLIANCE_RIGHT);
+  private static HumanLoadingStationHandler RED_ALLIANCE_LEFT =
+      new HumanLoadingStationHandler(BLUE_ALLIANCE_LEFT);
+
+  private HumanLoadingStationHandler(
+      PoseAndHeading finalPickupPose, PoseAndHeading... checkpointsOutsideIn) {
+
+    this.checkpointsOutsideIn = checkpointsOutsideIn;
+    this.finalPickupPose = finalPickupPose;
+  }
+
+  private HumanLoadingStationHandler(HumanLoadingStationHandler blueToFlip) {
+    this.finalPickupPose = flipForRed(blueToFlip.finalPickupPose);
+
+    ArrayList<PoseAndHeading> checkpointsList = new ArrayList<PoseAndHeading>();
+
+    for (PoseAndHeading poseAndHeading : blueToFlip.checkpointsOutsideIn) {
+      checkpointsList.add(poseAndHeading);
+    }
+
+    this.checkpointsOutsideIn = checkpointsList.toArray(new PoseAndHeading[0]);
+  }
+
+  private static PoseAndHeading flipForRed(PoseAndHeading toFlip) {
+    var oldPose = toFlip.pose;
+
+    Pose2d newPose =
+        new Pose2d(
+            oldPose.getX(),
+            GridPositionHandler.FIELD_WIDTH_METERS - oldPose.getY(),
+            oldPose.getRotation());
+
+    return new PoseAndHeading(newPose, toFlip.heading);
   }
 
   public static enum LoadingStationLocation {
@@ -59,7 +107,7 @@ public class HumanLoadingStationHandler {
       }
     }
 
-    return (PoseAndHeading[]) checkpoints.toArray();
+    return checkpoints.toArray(new PoseAndHeading[0]);
   }
 
   public static PoseAndHeading getFinalPoseForLocationAndAlliance(
@@ -82,5 +130,9 @@ public class HumanLoadingStationHandler {
       default:
         return null;
     }
+  }
+
+  public static void log(String path) {
+    // Logger.getInstance().recordOutput(path + "/BLUE_LEFT"/, value);
   }
 }
