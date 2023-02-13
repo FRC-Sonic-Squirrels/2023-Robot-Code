9ab21219e34332c6759e220332860c2af33273c7
LuminousLlama <keyushattarde@gmail.com>
2023-02-13 00:00:40 -0800

    more reliable auto from in front of pad

--- src/main/java/frc/lib/team2930/driverassist/EntranceCheckpoints.java
+++ src/main/java/frc/lib/team2930/driverassist/EntranceCheckpoints.java
@@ -10,20 +10,32 @@ public enum EntranceCheckpoints {
   // TODO: Add a to pathpoint function(add this to the other physical grid locations as well)
   BLUE_WALL(
       new PoseAndHeading(
-          new Pose2d(2.5, 0.7, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
+          new Pose2d(5.5, 0.7, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
+      new PoseAndHeading(
+          new Pose2d(5.2, 0.7, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
+      new PoseAndHeading(
+          new Pose2d(5.0, 0.7, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
+      new PoseAndHeading(
+          new Pose2d(4.8, 0.7, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
       new PoseAndHeading(
           new Pose2d(3.85, 0.7, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
       new PoseAndHeading(
-          new Pose2d(5.5, 0.7, Rotation2d.fromDegrees(180)),
+          new Pose2d(2.5, 0.7, Rotation2d.fromDegrees(180)),
           Rotation2d.fromDegrees(180))), // x:2.17
 
   BLUE_HUMAN_PLAYER(
       new PoseAndHeading(
-          new Pose2d(2.5, 4.65, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
+          new Pose2d(5.5, 4.65, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
+      new PoseAndHeading(
+          new Pose2d(5.2, 4.65, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
+      new PoseAndHeading(
+          new Pose2d(5.0, 4.65, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
+      new PoseAndHeading(
+          new Pose2d(4.8, 4.65, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
       new PoseAndHeading(
           new Pose2d(3.85, 4.65, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180)),
       new PoseAndHeading(
-          new Pose2d(5.5, 4.65, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180))),
+          new Pose2d(2.5, 4.65, Rotation2d.fromDegrees(180)), Rotation2d.fromDegrees(180))),
 
   // -----------------------------RED-----------------------
 
@@ -33,84 +45,81 @@ public enum EntranceCheckpoints {
 
   ERROR();
 
-  public final PoseAndHeading checkPointInside;
-  public final PoseAndHeading checkPointMiddle;
-  public final PoseAndHeading checkPointOutside;
-
   private static final String ROOT_TABLE = GridPositionHandler.ROOT_TABLE;
   private static final double FIELD_WIDTH_METERS = GridPositionHandler.FIELD_WIDTH_METERS;
 
-  private PoseAndHeading[] orderOutsideIn;
+  private final PoseAndHeading[] orderOutsideIn;
 
-  private EntranceCheckpoints(
-      PoseAndHeading inside, PoseAndHeading middle, PoseAndHeading outside) {
-    this.checkPointInside = inside;
-    this.checkPointMiddle = middle;
-    this.checkPointOutside = outside;
+  // private EntranceCheckpoints(
+  //     PoseAndHeading inside, PoseAndHeading middle, PoseAndHeading outside) {
+  //   this.checkPointInside = inside;
+  //   this.checkPointMiddle = middle;
+  //   this.checkPointOutside = outside;
 
-    orderOutsideIn = new PoseAndHeading[3];
-    orderOutsideIn[0] = outside;
-    orderOutsideIn[1] = middle;
-    orderOutsideIn[2] = inside;
+  //   orderOutsideIn = new PoseAndHeading[3];
+  //   orderOutsideIn[0] = outside;
+  //   orderOutsideIn[1] = middle;
+  //   orderOutsideIn[2] = inside;
+  // }
+
+  private EntranceCheckpoints(PoseAndHeading... orderOutsideIn) {
+    this.orderOutsideIn = orderOutsideIn;
   }
 
   private EntranceCheckpoints(EntranceCheckpoints blueToFlip) {
-    var checkPointsOutSideIn = blueToFlip.getOrderOutsideIn();
+    var blueCheckpoints = blueToFlip.orderOutsideIn;
 
-    PoseAndHeading[] redSideCheckpoints = new PoseAndHeading[3];
+    this.orderOutsideIn = new PoseAndHeading[blueCheckpoints.length];
 
-    for (int i = 0; i < checkPointsOutSideIn.length; i++) {
-      PoseAndHeading original = checkPointsOutSideIn[i];
+    for (int i = 0; i < blueCheckpoints.length; i++) {
+      var blueCheckpoint = blueCheckpoints[i];
+      var bluePose = blueCheckpoint.pose;
+      var blueHeading = blueCheckpoint.heading;
 
-      Pose2d alteredPose =
-          new Pose2d(
-              original.pose.getX(),
-              FIELD_WIDTH_METERS - original.pose.getY(),
-              original.pose.getRotation());
+      var redCheckpoint =
+          new PoseAndHeading(
+              new Pose2d(
+                  bluePose.getX(), FIELD_WIDTH_METERS - bluePose.getY(), bluePose.getRotation()),
+              blueHeading);
 
-      redSideCheckpoints[i] = new PoseAndHeading(alteredPose, original.heading);
-
-      this.orderOutsideIn = redSideCheckpoints;
+      this.orderOutsideIn[i] = redCheckpoint;
+      // PoseAndHeading redCheckpoint = new PoseAndHeading(new Pose2d(, y, rotation), heading)
     }
-
-    checkPointOutside = redSideCheckpoints[0];
-    checkPointMiddle = redSideCheckpoints[1];
-    checkPointInside = redSideCheckpoints[2];
-  }
-
-  private EntranceCheckpoints() {
-    this.checkPointInside = new PoseAndHeading();
-    this.checkPointMiddle = new PoseAndHeading();
-    this.checkPointOutside = new PoseAndHeading();
   }
 
   public PoseAndHeading[] getOrderOutsideIn() {
     return orderOutsideIn;
   }
 
-  public static PathPoint toPathPoint(PoseAndHeading selectedCheckPoint) {
+  public static PathPoint toPathPoint(PoseAndHeading selectedPoint) {
     return new PathPoint(
-        selectedCheckPoint.pose.getTranslation(),
-        selectedCheckPoint.heading,
-        selectedCheckPoint.pose.getRotation());
+        selectedPoint.pose.getTranslation(),
+        selectedPoint.heading,
+        selectedPoint.pose.getRotation());
   }
 
   public void log(String ROOT) {
-    Pose2d insidePose =
-        new Pose2d(checkPointInside.pose.getTranslation(), checkPointInside.heading);
-    Pose2d middlePose =
-        new Pose2d(checkPointMiddle.pose.getTranslation(), checkPointMiddle.heading);
-    Pose2d outsidePose =
-        new Pose2d(checkPointOutside.pose.getTranslation(), checkPointOutside.heading);
+    // Pose2d insidePose =
+    //     new Pose2d(checkPointInside.pose.getTranslation(), checkPointInside.heading);
+    // Pose2d middlePose =
+    //     new Pose2d(checkPointMiddle.pose.getTranslation(), checkPointMiddle.heading);
+    // Pose2d outsidePose =
+    //     new Pose2d(checkPointOutside.pose.getTranslation(), checkPointOutside.heading);
 
-    Logger.getInstance()
-        .recordOutput(
-            ROOT + "/EntranceCheckpoints/" + this.name() + "/insideCheckpoint", insidePose);
-    Logger.getInstance()
-        .recordOutput(
-            ROOT + "/EntranceCheckpoints/" + this.name() + "/middleCheckpoint", middlePose);
-    Logger.getInstance()
-        .recordOutput(
-            ROOT + "/EntranceCheckpoints/" + this.name() + "/outsideCheckpoint", outsidePose);
+    // Logger.getInstance()
+    //     .recordOutput(
+    //         ROOT + "/EntranceCheckpoints/" + this.name() + "/insideCheckpoint", insidePose);
+    // Logger.getInstance()
+    //     .recordOutput(
+    //         ROOT + "/EntranceCheckpoints/" + this.name() + "/middleCheckpoint", middlePose);
+    // Logger.getInstance()
+    //     .recordOutput(
+    //         ROOT + "/EntranceCheckpoints/" + this.name() + "/outsideCheckpoint", outsidePose);
+
+    for (int i = 0; i < orderOutsideIn.length; i++) {
+      Logger.getInstance()
+          .recordOutput(
+              ROOT + "/EntranceCheckpoints/" + this.name() + "/" + i, orderOutsideIn[i].pose);
+    }
   }
 }
