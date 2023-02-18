bdf81b6d404908c723a769ae062d8665f5399bc5
Soliana Aderaw <solianaby@gmail.com>
2023-02-17 16:58:07 -0800

    intake motor control

--- src/main/java/frc/robot/subsystems/intake/IntakeIO2023.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO2023.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrame;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class IntakeIO2023 implements IntakeIO {
@@ -8,10 +9,16 @@ public class IntakeIO2023 implements IntakeIO {
   WPI_TalonFX motor = new WPI_TalonFX(-1);
 
   public IntakeIO2023() {
+
     // TalonFXConfiguration config = new TalonFXConfiguration();
     motor.configFactoryDefault();
     motor.setInverted(false);
     motor.setNeutralMode(NeutralMode.Brake);
+    motor.configVoltageCompSaturation(10.0);
+    motor.enableVoltageCompensation(true);
+    motor.setStatusFramePeriod(StatusFrame.Status_1_General, 17);
+    motor.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 41);
+   
   }
 
   @Override
