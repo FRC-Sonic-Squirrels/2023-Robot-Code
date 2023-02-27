69b2800b5fa4c991f3b98eebc5c656e14817a582
Sonic Squirrels Programming <sonicsquirrels@gmail.com>
2023-02-26 16:02:29 -0800

    help hack to have code build

--- src/main/java/frc/robot/autonomous/SwerveAutos.java
+++ src/main/java/frc/robot/autonomous/SwerveAutos.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.lib.team2930.AutoChooserElement;
 import frc.robot.commands.auto.FollowPath;
@@ -314,28 +315,33 @@ public class SwerveAutos {
     return new AutoChooserElement(
         null,
         new SequentialCommandGroup(
-            MechanismPositions.scoreConeHighPosition(elevator, stinger, intake)));
+            MechanismPositions.scoreConeHighPosition(
+                // FIXME CREATE A VERSION THAT DOESNT NEED CONTROLLER
+                elevator, stinger, intake, new CommandXboxController(1))));
   }
 
   public AutoChooserElement scoreCubeHigh() {
     return new AutoChooserElement(
         null,
         new SequentialCommandGroup(
-            MechanismPositions.scoreCubeHighPosition(elevator, stinger, intake)));
+            MechanismPositions.scoreCubeHighPosition(
+                elevator, stinger, intake, new CommandXboxController(1))));
   }
 
   public AutoChooserElement scoreConeMid() {
     return new AutoChooserElement(
         null,
         new SequentialCommandGroup(
-            MechanismPositions.scoreCubeMidPosition(elevator, stinger, intake)));
+            MechanismPositions.scoreCubeMidPosition(
+                elevator, stinger, intake, new CommandXboxController(1))));
   }
 
   public AutoChooserElement scoreCubeMid() {
     return new AutoChooserElement(
         null,
         new SequentialCommandGroup(
-            MechanismPositions.scoreCubeMidPosition(elevator, stinger, intake)));
+            MechanismPositions.scoreCubeMidPosition(
+                elevator, stinger, intake, new CommandXboxController(1))));
   }
 
   public AutoChooserElement driveAutoEngage(Boolean flip) {
