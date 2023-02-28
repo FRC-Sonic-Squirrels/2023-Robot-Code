da8007c9775b1fc1101ca02c78774fbc288f1a5c
LuminousLlama <keyushattarde@gmail.com>
2023-02-28 00:07:49 -0800

    fix intake command in wrong folder

--- src/main/java/frc/robot/commands/intake/IntakeAutoGrabDesiredGamePiece.java
+++ src/main/java/frc/robot/commands/intake/IntakeAutoGrabDesiredGamePiece.java
@@ -2,13 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.intake;
+package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import frc.robot.RobotState;
 import frc.robot.RobotState.GamePiece;
-import frc.robot.commands.intake.IntakeGrabCone;
-import frc.robot.commands.intake.IntakeGrabCube;
+import frc.robot.subsystems.intake.Intake;
 
 public class IntakeAutoGrabDesiredGamePiece extends ConditionalCommand {
   public IntakeAutoGrabDesiredGamePiece(Intake intake) {
