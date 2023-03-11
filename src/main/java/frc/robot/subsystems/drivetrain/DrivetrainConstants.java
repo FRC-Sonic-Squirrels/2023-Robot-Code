2029a12eb00fef94498429499e0cc9a822d96cfb
Sonic Squirrels Programming <sonicsquirrels@gmail.com>
2023-03-10 21:03:36 -0800

    new offsets

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
@@ -19,25 +19,25 @@ public final class DrivetrainConstants {
   public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
   public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 11;
   public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 21;
-  public static final double FRONT_LEFT_MODULE_STEER_OFFSET = 113.6; // 103.8
+  public static final double FRONT_LEFT_MODULE_STEER_OFFSET = 221.4;
 
   // 1
   public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
   public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 12;
   public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 22;
-  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = 266.1; // 80.2;
+  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = 190.4;
 
   // Module 2
   public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4;
   public static final int BACK_LEFT_MODULE_STEER_MOTOR = 14;
   public static final int BACK_LEFT_MODULE_STEER_ENCODER = 24;
-  public static final double BACK_LEFT_MODULE_STEER_OFFSET = 240.2; // 14.2;
+  public static final double BACK_LEFT_MODULE_STEER_OFFSET = 240.7; // 14.2;
 
   // module 3
   public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 3;
   public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 13;
   public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 23;
-  public static final double BACK_RIGHT_MODULE_STEER_OFFSET = 249.4; // 201.9; // 201.7;
+  public static final double BACK_RIGHT_MODULE_STEER_OFFSET = 250.0; // 201.9; // 201.7;
 
   public static final int PIGEON_ID = 15;
   public static final String PIGEON_CAN_BUS_NAME = "CANivore";
