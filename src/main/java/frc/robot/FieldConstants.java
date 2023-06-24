a7674e6921e85f27e9794f18fb3879b84a6c98c6
LuminousLlama <keyushattarde@gmail.com>
2023-06-23 21:42:12 -0700

    create field constants file

---
+++ src/main/java/frc/robot/FieldConstants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public class FieldConstants {
+  // get field dimensions from official WPILib file
+  // https://github.com/wpilibsuite/allwpilib/blob/main/apriltag/src/main/native/resources/edu/wpi/first/apriltag/2023-chargedup.json#L148-L151
+  public static final double FIELD_LENGTH_METERS = 16.54175;
+  public static final double FIELD_WIDTH_METERS = 8.0137;
+}
